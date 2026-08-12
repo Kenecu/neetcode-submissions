03a1612ee991c5e20e4ee3dcba7e5c25bf6df85d
Kenny <164435131+Kenecu@users.noreply.github.com>
2026-08-12 16:11:08 -0400

    Add: is-palindrome - submission-17

---
+++ Data Structures & Algorithms/is-palindrome/submission-17.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        index1 = 0
+        index2 = len(s) - 1
+
+        while index1 < index2:
+            while index1 < index2 and not s[index1].isalnum():
+                    index1 += 1
+            while index1 < index2 and not s[index2].isalnum():
+                    index2 -= 1
+            if s[index1].lower() != s[index2].lower():
+                return False
+            index1 +=1
+            index2 -=1
+        return True
+
+
